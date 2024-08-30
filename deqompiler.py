cb7fc9e0de8c693c42161de005e6e522d7fe8c72
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2024-08-30 18:12:44 +0530

    start deqompiler

--- deqompiler.py
+++ deqompiler.py
@@ -1,248 +1,138 @@
-import ast
-import random
-from sympy import *
+from q_ast_gen import random_qiskit_ast_generator
 
-rotation_gates = ['rx', 'ry', 'rz', 'u1', 'u2', 'u3', 'crx', 'cry', 'crz', 'cp', 'cu1', 'cu3']
-multi_qubit_gates = ['cx', 'cz', 'swap', 'ch', 'csx', 'cy', 'ccx', 'cswap', 'cu', 'cp']
-three_qubit_gates = ['ccx', 'cswap']
-loop_prob = 0.99
+import os
+import time
+from tqdm import tqdm
 
-def random_expr(curr_loop_depth, max_expr_operators, var_depth):
+class gp_deqompiler:
     """
-    Generate a random expression using arithmetic, modulus, or simple variables.
-    This expr is used both for qubit indexing and phase
-
-    Args:
-        depth (int): Number of loop variables.
-        max_expr_operators (int): Number of binary operations to perform.
-        var_depth (int): Number of additional variables.
+    Genetic Programming based QASM-Qiskit Decompiler
+    Uses Qiskit AST Generator to generate initial population
     """
-    # Generate variable names for loop indices and variables
-    loop_vars = [f"i{ind}" for ind in range(curr_loop_depth)]
-    vars = ['n']+[f"{ind}" for ind in range(var_depth+1)]
+    def __init__(self, algorithm_name, qubit_limit=20, generations=100, pop_size=50, max_length=10, perform_crossover=True,
+                crossover_rate=0.3, new_gen_rate=0.2,mutation_rate=0.1,compare_method='l_by_l',max_loop_depth=2,
+                  perform_mutation=True, selection_method='tournament',operations = ['h', 'x', 'cx']):
+        self.algorithm_name = algorithm_name
+        self.qubit_limit = qubit_limit
+        self.generations = generations
+        self.pop_size = pop_size
+        self.max_length = max_length
+        self.crossover_rate=crossover_rate
+        self.mutation_rate=mutation_rate
+        self.new_gen_rate=new_gen_rate
+        self.max_loop_depth=max_loop_depth
+        self.perform_crossover = perform_crossover
+        self.compare_method=compare_method
+        self.perform_mutation = perform_mutation
+        self.selection_method = selection_method
+        self.operations=operations
+        # Initialize the path for saving files related to the algorithm
+        self.path = os.path.join('genetic_deQ', self.algorithm_name)
+        self.qasm_path=os.path.join('genetic_deQ_qasm', self.algorithm_name)
+        os.makedirs(self.path, exist_ok=True)  # Create the directory if it does not exist
+        os.makedirs(self.qasm_path, exist_ok=True) 
 
-    # All possible variables include 'n' and loop indices
-    choices = [ast.Name(id='n', ctx=ast.Load())] + [ast.Name(id=var, ctx=ast.Load()) for var in loop_vars ]
+    def run(self):
+        
+        best_score = float('-inf')
+        best_individual = None
+        best_generation_index = -1
+        best_individual_index = -1
+        best_code = ""
 
-    # Start with a random variable
-    # expr = ast.Name(id=random.choice(vars),ctx=ast.Load())
-    expr = random.choice(choices)
-    # Add binary operations
-    for _ in range(max_expr_operators+1):
-        left = expr
-        right = ast.Name(id=random.choice(vars),ctx=ast.Load())
-        op = random.choice([ast.Add(), ast.Sub()])
-        expr = ast.BinOp(left=left, op=op, right=right)
-    return expr    
+        # Generate initial population once at the beginning
+        population = []
+        for _ in range(self.pop_size):
+            ast_circuit = random_qiskit_ast_generator(operations=self.operations,max_num_nodes=self.max_length,max_loop_depth=self.max_loop_depth)
+            population.append(ast_circuit)
 
-def random_qubit_expr(curr_loop_depth, num_ops, num_vars):
-    
-    qubit_expr = random_expr(curr_loop_depth, num_ops, num_vars)
-    mod_qubit_expr = ast.BinOp(left=qubit_expr, op=ast.Mod(), right=ast.Name(id='n', ctx=ast.Load()))   # Apply modulo operation to ensure the result is within valid index range
-    sympl_qubit_expr = ast.parse(str(simplify(ast.unparse(mod_qubit_expr)))).body[0].value
-    # print("Simplified expression:",ast.unparse(mod_qubit_expr),"--->",ast.unparse(sympl_qubit_expr))
-    return sympl_qubit_expr
+        for generation in range(self.generations):
+            start_time = time.time()
 
-def random_positive_gaussian_integers(mu=0, sigma=1):
-    """Generate positive random integers from a Gaussian distribution, ensuring all numbers are within a given range [1, upper_bound].
-    
-    Args:
-        mu (float): Mean of the Gaussian distribution.
-        sigma (float): Standard deviation of the Gaussian distribution.
-        num_samples (int): Number of samples to generate.
-        upper_bound (int): Maximum value of the random integer (inclusive).
-    """
-    # Generate number, take absolute value, round, and apply upper bound
-    number = random.gauss(mu, sigma)
-    positive_integer = int(abs(number))
-    return positive_integer
+        #     # Save the current population state and QASM data
+        #     self.save(population)
+        #     self.save_qasm()
 
-def random_phase_expr(depth):
-    """
-    Generate a random phase expression of the form pi * 1 / (2**a + b + c).
-    """
- 
-    a = random_expr(depth,depth,0)
-    b = random_expr(depth,depth,0)
-    c = ast.Constant(value=random_positive_gaussian_integers())
-    
-    # Create the expression 2**a + b + c
-    expr_inner = ast.BinOp(
-        left=ast.BinOp(
-            left=ast.Constant(value=2),
-            op=ast.Pow(),
-            right=a
-        ),
-        op=ast.Add(),
-        right=ast.BinOp(
-            left=b,
-            op=ast.Add(),
-            right=c
-        )
-    )
+        #     # Evaluate fitness for each individual
+        #     # [print(index) for index, individual in enumerate(population)]
+        #     fitness_scores = [self.evaluate(individual, index) for index, individual in enumerate(population)]
+        #     print("Fitness scores:", fitness_scores)  # Debugging line
 
-    # Create the expression pi * 1 / (2**a + b + c)
-    phase_expr = ast.BinOp(
-        left=ast.Name(id='pi', ctx=ast.Load()),
-        op=ast.Mult(),
-        right=ast.BinOp(
-            left=ast.Constant(value=1),
-            op=ast.Div(),
-            right=expr_inner
-        )
-    )
+        #      # Sort population by fitness (descending order)
+        #     sorted_population = sorted(zip(fitness_scores, population), key=lambda pair: pair[0], reverse=True)
+        #     sorted_scores, next_generation = zip(*sorted_population)
+        #     next_generation = list(next_generation)
+        #     sorted_scores = list(sorted_scores)
 
-    sympl_phase_expr = ast.parse(str(simplify(ast.unparse(phase_expr)))).body[0].value
-    return sympl_phase_expr
-
-def loop_index(depth):
-    if depth == 0:
-        return ast.Name(id='n', ctx=ast.Load())
-    else:
-        # Generate variable names for loop indices
-        vars = [f"i{ind}" for ind in range(depth)]
-        choices = [ast.Name(id='n', ctx=ast.Load())]+\
-        [ast.Name(id=var, ctx=ast.Load()) for var in vars]
-
-        # Start with a random variable
-        expr = random.choice(choices)
-
-        # Add binary operations
-        for _ in range(depth):
-            left = expr
-            right = random.choice(choices)
-            op = random.choice([ast.Add(), ast.Sub()])
-            expr = ast.BinOp(left=left, op=op, right=right)
-
-        # Random value to add/subtract
-        value = random.randint(0, depth)  # Using a random integer instead of string
-        index = ast.BinOp(left=expr, op=random.choice([ast.Add(), ast.Sub()]), 
-                          right=ast.Constant(value=value))
-
-        return ast.Call(
-            func=ast.Name(id='abs', ctx=ast.Load()),
-            args=[index],
-            keywords=[])
-    
-def random_qiskit_ast_gate(operations, curr_loop_depth, num_ops, num_vars):
-    gate = random.choice(operations)
-    index1 = random_qubit_expr(curr_loop_depth, num_ops, num_vars)
-
-    if gate in multi_qubit_gates:
-        index2 = random_qubit_expr(curr_loop_depth, num_ops, num_vars)
-        if gate in rotation_gates:
-            phase = random_phase_expr(curr_loop_depth)
-            gate_call = ast.Expr(value=ast.Call(
-                func=ast.Attribute(value=ast.Name(id="qc", ctx=ast.Load()), attr=gate, ctx=ast.Load()),
-                args=[phase, index1, index2],
-                keywords=[]
-            ))
-        else:
-            gate_call = ast.Expr(value=ast.Call(
-                func=ast.Attribute(value=ast.Name(id="qc", ctx=ast.Load()), attr=gate, ctx=ast.Load()),
-                args=[index1, index2],
-                keywords=[]
-            ))
-    else:
-        if gate in rotation_gates:
-            phase = random_phase_expr(curr_loop_depth)
-            gate_call = ast.Expr(value=ast.Call(
-                func=ast.Attribute(value=ast.Name(id="qc", ctx=ast.Load()), attr=gate, ctx=ast.Load()),
-                args=[phase, index1],
-                keywords=[]
-            ))
-        else:
-            gate_call = ast.Expr(value=ast.Call(
-                func=ast.Attribute(value=ast.Name(id="qc", ctx=ast.Load()), attr=gate, ctx=ast.Load()),
-                args=[index1],
-                keywords=[]
-            ))
-    return gate_call
-
-def random_qiskit_ast_body(body, operations, num_nodes, max_loop_depth, curr_loop_depth = 0):
-    """
-    Generate a random quantum algorithm body.
-    operations: List of quantum operations to choose from.
-    num_nodes: Number of lines in level 0. for loops (along with their body) are counted as 1 line.
-    max_loop_depth: Maximum depth of nested loops.
-    """
-
-    for i in range(num_nodes):
-        if random.random() < loop_prob:       # Randomly decide node is a loop or a gate operation
-
-            loop_body = []    
-            loop_depth = random.randint(1, max_loop_depth)
-            loop_vars = [f"i{ind}" for ind in range(loop_depth)]
-            current_body = loop_body  # Initialize current_body to loop_body
-            for j in range(loop_depth):
-                loop = ast.For(
-                    target=ast.Name(id=loop_vars[j], ctx=ast.Store()),
-                    iter=ast.Call(func=ast.Name(id='range', ctx=ast.Load()), args=[loop_index(j)], keywords=[]),  # Use loop_index here
-                    body=[],
-                    orelse=[]
-                )
-                current_body.append(loop)   # Append loop to the current body
-                current_body = loop.body    # Update current_body to the new loop's body
-
-            current_body.append(random_qiskit_ast_gate(operations, loop_depth, 2, 3))
-            body.extend(loop_body)
+        #     # Select the best individual and corresponding score
+        #     best_individual = next_generation[0]
+        #     best_score = sorted_scores[0]
             
-        else:
-            body.append(random_qiskit_ast_gate(operations, curr_loop_depth, 1, 2))
+        #     # Find the index of the best individual in the original population
+        #     best_individual_index = fitness_scores.index(best_score)
+            
+        #     # Print debugging information
+        #     print(f"Generation {generation + 1}: Best score = {best_score}")
 
-    return body
+        #     # If the best score is 1, stop the iteration
+        #     if best_score == 1:
+        #         break
+        #     new_population = []
 
-def random_qiskit_ast_generator(operations, num_nodes, max_loop_depth):
+        #     # Number of individuals to be generated by each method
+        #     crossover_count = int(self.pop_size * self.crossover_rate) if self.perform_crossover == True else 0
+        #     mutation_count = int(self.pop_size * self.mutation_rate) if self.perform_mutation == True else 0
+        #     new_gen_count = int(self.pop_size * self.new_gen_rate)
+        #     elite_count = self.pop_size - crossover_count - mutation_count - new_gen_count
 
-    import_def = [
-        ast.ImportFrom(module='qiskit', names=[ast.alias(name='QuantumCircuit', asname=None)], level=0),
-        ast.ImportFrom(module='math', names=[ast.alias(name='pi', asname=None)], level=0),
-        ast.Import(names=[ast.alias(name='numpy', asname='np')]),
-        ast.Import(names=[ast.alias(name='random', asname=None)])
-    ]
-    
-    args = ast.arguments(
-        posonlyargs=[],
-        args=[ast.arg(arg='n', annotation=None)],
-        vararg=None,
-        kwonlyargs=[],
-        kw_defaults=[],
-        kwarg=None,
-        defaults=[]
-    )
+        #     # Preserve elite individuals
+        #     new_population.extend(next_generation[:elite_count])
 
-    body = [
-        ast.Assign(
-            targets=[ast.Name(id="qc", ctx=ast.Store())],
-            value=ast.Call(
-                func=ast.Name(id='QuantumCircuit', ctx=ast.Load()),
-                args=[ast.Name(id='n', ctx=ast.Load())],
-                keywords=[]
-            )
-        )
-    ]
+        #     # Apply crossover to generate new individuals
+        #     while len(new_population) < elite_count + crossover_count:
+        #         parent1, parent2 = self.select_parents(next_generation, sorted_scores, self.selection_method)
+                
+        #         child1, child2 = self.crossover(parent1, parent2)
+               
+        #         new_population.extend([child1, child2])
 
-    body = random_qiskit_ast_body(body, operations, num_nodes, max_loop_depth)
-    
-    body.append(ast.Return(value=ast.Name(id="qc", ctx=ast.Load())))
+        #     # Apply mutation to new individuals
+        #     for _ in range(mutation_count):
+        #         if new_population:
+        #             individual_to_mutate = random.choice(new_population)
+        #             new_population.append(self.mutate(individual_to_mutate))
+            
+        #     # Generate new individuals
+        #     new_population.extend(self.generate_initial_population(new_gen_count))
 
-    function_def = ast.FunctionDef(
-        name="quantum_algorithm",
-        args=args,
-        body=body,
-        decorator_list=[],          # Decorators
-        returns=None,               # Annotate function's return type
-        type_comment=None
-    )
 
-    module = ast.Module(body=[import_def, function_def], type_ignores=[])
-    ast.fix_missing_locations(module)
-    return module
+        #     # Ensure the population size is correct after all operations
+        #     # new_population = new_population[:self.pop_size]
+
+        #     population = new_population
+
+            end_time = time.time()
+            time_taken = end_time - start_time
+            tqdm.write(f"Generation {generation + 1}/{self.generations} completed in {time_taken:.2f} seconds")
+
+        # # Unparse the AST of the best individual if found
+        # best_code = ast.unparse(best_individual) if best_individual else "No best individual found"
+        return best_code, best_score, best_individual_index
 
 if __name__ == "__main__":
 
-    operations = ['h', 'x', 'cx', 'rx', 'ry', 'rz']
-    module = random_qiskit_ast_generator(operations=operations, num_nodes=2, max_loop_depth=2)    # max_loop_depth=0 sometimes gives error
-    print(ast.unparse(module))
-    # print()
-    # pprint(ast.dump(module, indent=4))
+    operations = ['h', 'x', 'rx', 'ry', 'rz']
+    generations = 10
+    algorithm_name = 'rx_c'
+    compare_method = 'seq_similarity'
+    perform_crossover = False
+    perform_mutation = True
+    pop_size = 100
+    new_gen_rate = 0.6
+    
+    decompiler = gp_deqompiler(operations=operations,generations=generations,algorithm_name=algorithm_name,compare_method=compare_method,
+                                perform_crossover=perform_crossover,perform_mutation=perform_mutation,pop_size=pop_size,new_gen_rate=new_gen_rate)
+    
+    best_code, best_score, best_generation_index = decompiler.run()
+    
+    print(best_code, '\n', best_score, '\n',best_generation_index)
